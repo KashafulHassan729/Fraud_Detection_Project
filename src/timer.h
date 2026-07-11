5eb4f3d8ed34cd8df851d77c46439ea333b48fa7
Kashaf ul Hassan <231400102@gift.edu.pk>
2026-07-11 19:58:57 +0500

    Fix duplicated content in timer.h

--- src/timer.h
+++ src/timer.h
@@ -19,25 +19,4 @@ private:
     std::chrono::high_resolution_clock::time_point start_time;
 };
 
-#endif#ifndef TIMER_H
-#define TIMER_H
-
-#include <chrono>
-
-class Timer {
-public:
-    void start() {
-        start_time = std::chrono::high_resolution_clock::now();
-    }
-
-    double stop_ms() {
-        auto end_time = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> elapsed = end_time - start_time;
-        return elapsed.count();
-    }
-
-private:
-    std::chrono::high_resolution_clock::time_point start_time;
-};
-
 #endif
